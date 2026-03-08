b1ecd564d0c43e2e13aa31d8dcea7451745d9fcb
akash__dp <117759810+DP7772@users.noreply.github.com>
2026-03-08 17:57:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
